a5414bf8552fcba5024f8ac0b094d5968701ac26
alvaro-freire <alvaro.freirea@udc.es>
2022-05-05 22:32:26 +0200

    start design (shit yet)

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import Image from 'next/image'
-
-export default function Home() {
+function Home() {
   return (
-    <div className='text-center h-screen bg-slate-200'>
-      <div>
-        <Image alt='logo' src={'/apple-touch-icon.png'} width='60px' height={'60px'} />
+    <>
+      <title>alvarofreire.es</title>
+      <div className='bg-[#0D5C63] h-screen flex flex-col justify-center'>
+        <div className='p-4 border rounded mx-auto w-[350px] bg-[#44A1A0]'>
+          <div className='p-4 bg-[#78CDD7]'>
+          <p className='text-center w-full'>
+            Hi! My name is Álvaro. I'm a CS
+            student at Universidade da Coruña.
+          </p>
+          <p className='text-center w-full'>
+            I'm a big fan of everything related
+            to computers and always trying to
+            learn new things!
+          </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
+
+export default Home
