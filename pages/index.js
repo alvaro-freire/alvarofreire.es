2f37152eaad8928e33916379dc249a40e15bcc70
alvaro-freire <alvaro.freirea@udc.es>
2022-05-06 19:06:15 +0200

    fixed ' error

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,15 @@ function Home() {
       <div className='bg-[#0D5C63] h-screen flex flex-col justify-center'>
         <div className='p-4 border rounded mx-auto w-[350px] bg-[#44A1A0]'>
           <div className='p-4 bg-[#78CDD7]'>
-          <p className='text-center w-full'>
-            Hi! My name is Álvaro. I'm a CS
-            student at Universidade da Coruña.
-          </p>
-          <p className='text-center w-full'>
-            I'm a big fan of everything related
-            to computers and always trying to
-            learn new things!
-          </p>
+            <p className='text-center w-full'>
+              Hi! My name is Álvaro. I&apos;m a CS
+              student at Universidade da Coruña.
+            </p>
+            <p className='text-center w-full'> 
+              I&apos;m a big fan of everything related
+              to computers and always trying to
+              learn new things!
+            </p>
           </div>
         </div>
       </div>
