0a9fd3cc2e590be639a51db6e9722a76a2788b27
alvaro-freire <alvaro.freirea@udc.es>
2022-05-06 19:24:53 +0200

    some design -improvements-

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Seo from "../components/seo"
 
-
 function Home() {
   return (
     <>
       <Seo/>
       <div className='bg-[#0D5C63] h-screen flex flex-col justify-center'>
-        <div className='p-4 border rounded mx-auto w-[350px] bg-[#44A1A0]'>
-          <div className='p-4 bg-[#78CDD7]'>
-            <p className='text-center w-full'>
+        <div className='p-4 border border-black rounded mx-auto w-[500px] h-56'>
+          <div className='p-4 h-full text-[#44A1A0]'>
+            <p className='text-center text-xl font-mono w-full'>
               Hi! My name is Álvaro. I&apos;m a CS
               student at Universidade da Coruña.
             </p>
-            <p className='text-center w-full'> 
+            <p className='text-center text-sm font-mono w-full mt-14'> 
               I&apos;m a big fan of everything related
               to computers and always trying to
               learn new things!
