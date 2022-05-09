6f0b409b57dc6878079f58f61821edcababf6c28
alvaro-freire <alvaro.freirea@udc.es>
2022-05-09 13:49:19 +0200

    change color palette

--- components/socialmediaicon.js
+++ components/socialmediaicon.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 function SocialMediaIcon({ name, url, img }) {
   return (
     <Link href={url} >
-      <a target='_blank' className='transition-colors hover:text-[#fff]' >
+      <a target='_blank' className='transition-colors hover:text-[#CFB495]' >
         <div className='flex' >
           <Image alt='socialmedia' src={img} width={16} height={16} />
           <p className='ml-1'>{name}</p>
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <>
       <Seo />
-      <div className='bg-[#0D5C63] h-screen text-[#44A1A0] font-mono'>
+      <div className='bg-[#F5CDAA] h-screen text-[#5D5B6A] font-mono'>
         <div className='flex flex-col sm:w-[640px] h-full mx-auto'>
           <div className='h-[100px]'>
 
@@ -14,15 +14,15 @@ function Home() {
           <div className='grow text-center px-10 flex flex-col justify-center'>
             <div>
               <p className='text text-3xl'>
-                Hi! My name is <span className='text-[#75cbca]'>Álvaro</span>.
+                Hi! My name is <span className='text-[#ad957a]'>Álvaro</span>.
               </p>
               <p className='text-md mt-10 mx-4'>
                 I&apos;m a passionate developer currently
                 based in <Link href='https://www.google.com/maps/place/La+Coru%C3%B1a/@43.3618688,-8.4477032,13z/data=!3m1!4b1!4m5!3m4!1s0xd2e7cfcf174574d:0x6a47350d095cdfee!8m2!3d43.3623436!4d-8.4115401'>
-                  <a target='_blank' className='underline transition-colors hover:text-[#fff]' >A Coruña</a>
+                  <a target='_blank' className='underline transition-colors hover:text-[#CFB495]' >A Coruña</a>
                 </Link>. I&apos;m really into JavaScript,
                 React and Python and building cool products with them - I like writing <Link href='https://github.com/alvaro-freire'>
-                  <a target='_blank' className='underline transition-colors hover:text-[#fff]' >code</a>
+                  <a target='_blank' className='underline transition-colors hover:text-[#CFB495]' >code</a>
                 </Link> :)
               </p>
             </div>
