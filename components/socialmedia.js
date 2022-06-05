f445fdf4bb99a96d19d2c1d18ca94374adae59fb
Álvaro Freire Ares <76497873+alvaro-freire@users.noreply.github.com>
2022-06-05 10:34:14 +0200

    Update socialmedia.js

--- components/socialmedia.js
+++ components/socialmedia.js
@@ -2,7 +2,7 @@ import SocialMediaIcon from "./socialmediaicon"
 
 function Socialmedia() {
   const socialLinks = [
-    { name: 'LinkedIn', url: 'https://linkedin.com/in/álvaro-freire-ares-052a001b1/', img: '/linkedin-svgrepo-com.svg' },
+    { name: 'LinkedIn', url: 'https://linkedin.com/in/alvvarofreire/', img: '/linkedin-svgrepo-com.svg' },
     { name: 'GitHub', url: 'https://github.com/alvaro-freire', img: '/github-svgrepo-com.svg' },
     { name: 'Twitter', url: 'https://twitter.com/alvvarofreire', img: '/twitter-svgrepo-com.svg' },
     { name: 'Instagram', url: 'https://instagram.com/alvvarofreire', img: '/instagram-svgrepo-com.svg' }
