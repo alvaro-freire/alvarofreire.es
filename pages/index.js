7e44c14330fc341102e1ee94cdd3292dff228a64
Álvaro Freire <alvaro.freirea@udc.es>
2023-02-15 14:02:07 +0100

    new UI for Erasmus+ project

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,27 @@ function Home() {
   return (
     <>
       <Seo />
-      <div className='bg-[#F5CDBB] h-screen text-[#5D5B6A] font-mono'>
-        <div className='flex flex-col sm:w-[640px] h-full mx-auto'>
-          <div className='h-[100px]'>
-
-          </div>
-          <div className='grow text-center px-10 flex flex-col justify-center'>
+      <div className='bg-[#F5CDBB] min-h-[100vh] max-w-[100vw] text-[#5D5B6A] font-mono flex flex-col justify-center'>
+        <div className='flex py-12 flex-col sm:max-w-[600px] h-full mx-auto px-4'>
+          <div className='grow flex flex-col justify-center'>
             <div>
-              <p className='text text-3xl'>
-                Hi! My name&apos;s <span className='text-[#ad957a]'>Álvaro</span>.
+              <p className='text-2xl text-center'>
+                Hey, hey, hey! Look who we have here! You made it to my page and I couldn&apos;t be more excited 😁
               </p>
-              <p className='text-md mt-10 mx-4'>
-                I&apos;m a passionate developer based in <Link href='https://www.google.com/maps/place/La+Coru%C3%B1a/@43.3618688,-8.4477032,13z/data=!3m1!4b1!4m5!3m4!1s0xd2e7cfcf174574d:0x6a47350d095cdfee!8m2!3d43.3623436!4d-8.4115401'>
-                  <a target='_blank' className='transition-colors hover:text-[#CFB495]'>A Coruña</a>
-                </Link>, currently @ <Link href='https://innogando.com'>
-                  <a target='_blank' className='transition-colors hover:text-[#CFB495]'>Innogando</a>
-                </Link>. I&apos;m really into JavaScript,
-                React and Python and building cool products with them. Here&apos;s some of my <Link href='https://github.com/alvaro-freire'>
-                  <a target='_blank' className='underline transition-colors hover:text-[#CFB495]' >code</a>
-                </Link> :)
+              <p className='text-center text-lg py-10 mx-4'>
+                Here are some interesting things I can tell  you about me:
+              </p>
+              <p className='text-md mx-4'>
+                - My name&apos;s <span className='text-[#d17b56]'>Álvaro</span> &#40;🇪🇸 Team&#41; <br/><br/>
+                - I&apos;m <span className='text-[#d17b56]'>21 years old</span>, right now finishing my studies in <span className='text-[#d17b56]'>Computer Engineering</span> 💻. <br/><br/>
+                - I consider myself as a person who loves getting out of his <span className='text-[#d17b56]'>comfort-zone</span>, setting <span className='text-[#d17b56]'>challenges</span> 🏁 to himself and learning new things about any topic. <br/><br/>
+                - I&apos;m a big fan of traveling <span className='text-[#d17b56]'>all around the world</span> 🌍 - right now I have been to <span className='text-[#d17b56]'>10 countries</span>! <br/> <br/>
+                - I have participated in <span className='text-[#d17b56]'>3 Erasmus+ YE</span> 🇪🇺 so far and co-wrote one of them! <br/><br/>
+                - I co-founded a youth club 👦👧 in my hometown. <br/><br/>
+                - I&apos;m very enthusiastic for <span className='text-[#d17b56]'>meeting new people</span>, you never know who can inspire 💭 your or make an impact 💡 in your life! <br/><br/>
+                - I&apos;m a big fan of practicing every kind of sport. I have practiced  <span className='text-[#d17b56]'>9 different sports</span> ⚽🏀🎾🥋 - at least that I remember, hehe.
               </p>
             </div>
-          </div>
-          <div className='h-[100px] flex flex-col justify-center'>
-            <SocialMedia />
           </div>
         </div>
       </div>
